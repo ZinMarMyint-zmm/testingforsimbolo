ba323f2bd738962dd109f9458dbf7eaf2d55a180
Zin Mar Myint <99066845+ZinMarMyint-zmm@users.noreply.github.com>
2025-02-05 21:40:58 +0630

    Add files via upload

---
+++ streamlit_demo.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import OpenAI
+
+with st.sidebar:
+    messages = st.container(height=300)
+    if prompt := st.chat_input("Say something"):
+        messages.chat_message("user").write(prompt)
+        messages.chat_message("assistant").write(f"echo: {prompt}")
+    
